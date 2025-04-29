f1c4cd4d1b08bd68af9d5f14eeaba95bcacad3ee
pedro-digid <98165692+pedro-digid@users.noreply.github.com>
2025-04-29 11:34:28 +0100

    up

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -31,11 +31,8 @@ export function AppSidebar({ ...props }) {
           <SidebarMenuItem>
             <SidebarMenuButton size="lg" asChild>
               <a href="/dashboard">
-                <div className="bg-sidebar-primary text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-lg">
-                  <GalleryVerticalEnd className="size-4" />
-                </div>
                 <div className="grid flex-1 text-left text-sm leading-tight">
-                  <span className="truncate font-medium">Erlenhof</span>
+                  <img src="/erlenhof-zentrum-logo.svg" alt="Erlenhof Zentrum Logo" width="150" height="50" />
                   <span className="truncate text-xs">Ressourcenpool</span>
                 </div>
               </a>
